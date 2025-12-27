59e41996226609dabb98ef9907d29a024f52637f
tanman123kap <tanman123kap@gmail.com>
2025-12-27 18:04:52 +0530

    Update Add.jsx

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -35,7 +35,11 @@ const Add = ({token}) => {
             image3 && formData.append("image3", image3);
             image4 && formData.append("image4", image4);
 
-            const response = await axios.post(backendUrl + "/api/product/add", formData, {headers: {token}});
+            const response = await axios.post(backendUrl + "/api/product/add", formData, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
             if(response.data.success) {
                 toast.success(response.data.message);
                 setName("");
