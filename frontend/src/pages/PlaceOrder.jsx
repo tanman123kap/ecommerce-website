5f1800fe7ffba244dc9362876754de83e0fcf83b
tanman123kap <tanman123kap@gmail.com>
2025-12-27 18:03:36 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -36,7 +36,11 @@ const PlaceOrder = () => {
       receipt: order.receipt,
       handler: async (response) => {
         try {
-          const {data} = await axios.post(backendUrl + "/api/order/verifyRazorpay", response, {headers: {token}});
+          const {data} = await axios.post(backendUrl + "/api/order/verifyRazorpay", response,{
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
           if(data.success) {
             navigate("/orders");
             setCartItem({});
@@ -72,7 +76,11 @@ const PlaceOrder = () => {
       }
       switch(method) {
         case "COD":
-          const response = await axios.post(backendUrl + "/api/order/place", orderData, {headers: {token}});
+          const response = await axios.post(backendUrl + "/api/order/place", orderData, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
           if(response.data.success) {
             setCartItem({});
             navigate("/orders");
@@ -81,7 +89,11 @@ const PlaceOrder = () => {
           }
           break;
         case "stripe":
-          const responseStripe = await axios.post(backendUrl + "/api/order/stripe", orderData, {headers: {token}});
+          const responseStripe = await axios.post(backendUrl + "/api/order/stripe", orderData, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
           if(responseStripe.data.success) {
             const {session_url} = responseStripe.data;
             window.location.replace(session_url);
@@ -90,7 +102,11 @@ const PlaceOrder = () => {
           }
           break;
         case "razorpay":
-          const responseRazorpay = await axios.post(backendUrl + "/api/order/razorpay", orderData, {headers: {token}});
+          const responseRazorpay = await axios.post(backendUrl + "/api/order/razorpay", orderData, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
           if(responseRazorpay.data.success) {
             initPay(responseRazorpay.data.order);
           } else {
